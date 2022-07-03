abe7b0d84ab60710ccd8df6addd12eaccffc03a1
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-03 12:00:49 +0300

    finish task 3

--- js/task-02.js
+++ js/task-02.js
@@ -14,13 +14,10 @@ const ingredients = [
 ? Добавит элементу класс item.
 ? После чего вставит все <li> за одну операцию в список ul#ingredients.
 */
+const ulRef = document.querySelector("#ingredients");
 for (const ingredient of ingredients) {
-  const ulRef = document.querySelector("#ingredients");
   const liRef = document.createElement("li");
   liRef.classList = "item";
   liRef.textContent = ingredient;
-  // console.log(liRef);
-
   ulRef.append(liRef);
-  // console.log(ulRef);
 }
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,33 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+/*
+? Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+? Используй массив объектов images для создания элементов <img> вложенных в <li>. 
+? Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+? Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+? Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+*/
+const ulRef = document.querySelector(".gallery");
+// console.log(ulRef);
+images.forEach((element) => {
+  ulRef.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <li>
+      <img src="${element.url}" alt = "${element.alt}" width = "300" height = "150"></img>
+    </li>`
+  );
+});
