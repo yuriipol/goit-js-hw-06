1049d24aaf7e42cc88871e83a68a4bfcfff8928a
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-05 16:13:59 +0300

    add text on redme file

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
+/*
+? Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на 
+? button.change-color и выводит значение цвета в span.color.
+
+<div class="widget">
+  <p>Background color: <span class="color">-</span></p>
+  <button type="button" class="change-color">Change color</button>
+</div>
+? Для генерации случайного цвета используй функцию getRandomHexColor.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+*/
+const backgroundVidjet = document.querySelector(".widget");
+const btnChangeColor = backgroundVidjet.querySelector(".change-color");
+const textNameColor = backgroundVidjet.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
